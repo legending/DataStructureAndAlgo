0fbee25cce0d2b7194ceec4498947799620a5e8e
Robin Wang <510471194@qq.com>
2020-10-26 16:13:14 +0800

    revised ShellSort and added comparator to verify the corretness of sorting algos

--- src/main/java/com/legend/dsandalgo/sort/RunTest.java
+++ src/main/java/com/legend/dsandalgo/sort/RunTest.java
@@ -3,9 +3,64 @@ package com.legend.dsandalgo.sort;
 import java.util.Arrays;
 
 public class RunTest {
+
     public static void main (String [] args) {
-        int [] arr = {123, 838, 2, 87, 834949, 0, 6667, 11, 101, 555, 12, 87, 45};
-        new ShellSort().sort(arr);
-        System.out.println(Arrays.toString(arr));
+        //利用对数器证明排序方法的正确性
+        int tryTimes = 1000000;
+        boolean isAllPassed = true;
+        for (int i = 0; i < tryTimes; i++) {
+            int maxSize = 200;
+            int maxValue = 1234567;
+            int [] arr1 = generateRandomArr(maxSize, maxValue);
+            int [] arr2 = copyArray(arr1);
+            //System.out.println("arr1 before: " + Arrays.toString(arr1));
+            //System.out.println("arr2 before: " + Arrays.toString(arr2));
+            //System.out.println("before：arr1 is " + (isEqual(arr1,arr2) ? "" : "not") + " equal to arr2");
+            new ShellSort().sort(arr1);
+            rightSort(arr2);
+            //System.out.println("arr1 after: " + Arrays.toString(arr1));
+            //System.out.println("arr2 after: " + Arrays.toString(arr2));
+            boolean isEqual = isEqual(arr1, arr2);
+            //System.out.println("final: arr1 is " + (isEqual? "" : "not") + " equal to arr2");
+            if (!isEqual) {
+                System.out.println("Not equal happened!!!!!!!!!!!!!!");
+                isAllPassed = false;
+                break;
+            }
+        }
+        if (isAllPassed) System.out.println("All passed");
+    }
+
+    //对数器
+    public static void rightSort(int [] arr) {
+        Arrays.sort(arr);
+    }
+
+    public static boolean isEqual(int[] a, int[] b) {
+        if (a == null && b == null) return true;
+        if (a == null && b != null || a != null && b == null || a.length != b.length) return false;
+        boolean isEqual = true;
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] != b[i]) isEqual=false;
+        }
+        return isEqual;
+    }
+
+    public static int[] generateRandomArr(int maxSize, int maxValue) {
+        int len = (int) (Math.random()*(maxSize+1));
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = (int)(Math.random()*(maxValue+1)) - (int)(Math.random()*maxValue);
+        }
+        return arr;
+    }
+
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) return null;
+        int[] retArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            retArr[i] = arr[i];
+        }
+        return retArr;
     }
 }
--- src/main/java/com/legend/dsandalgo/sort/ShellSort.java
+++ src/main/java/com/legend/dsandalgo/sort/ShellSort.java
@@ -2,24 +2,33 @@ package com.legend.dsandalgo.sort;
 
 /**
  * 希尔排序
- *  实质上是一种分组插入方法
- *  步骤
+ * 实质上是一种分组增量插入排序
+ * <p>
  * （1）第一层循环，将数组分割为间隔为gap的分区，直到gap=1
- * （2）每个固定的gap，依次次从下标gap到arr.length-1，以gap为前进单位
- * （3）判断是否需要交换
+ * （2）第二层循环，在某个gap下，依次从下标gap到arr.length-1，以gap为前进单位
+ * （3）在数组下标不断往前走的过程中，不断寻找间隔为gap的数组成一个组，并排序
  *
+ * 注意：gap/=2，gap必须是每次除2，否则会出问题
+ *
+ * 对有序序列插入有交换法和移动法，交换法效率较低，容易理解。移动法效率较高，不好理解。
  * https://www.cnblogs.com/chengxiao/p/6104371.html
  */
 public class ShellSort implements IArraySort {
 
+    public static void main(String[] args) {
+        int[] arr = {14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        new ShellSort().sort(arr);
+    }
+
     //希尔排序 针对有序序列在插入时采用交换法
     public void sort(int[] arr) {
         //增量gap，并逐步缩小增量
-        for (int gap = arr.length / 3; gap > 0; gap /= 3) {
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
             //从第gap个元素，逐个对其所在组进行直接插入排序操作
             for (int i = gap; i < arr.length; i++) {
+                System.out.print("gap=" + gap + ",i=" + i + ":");
                 int j = i;
-                while (j - gap >= 0 && arr[j] < arr[j - gap]) {
+                while (j - gap >= 0 && arr[j] < arr[j - gap]) {//在某个gap下，分在一组里面的数，在同一组下如何保证这些分组的数不重复？->组与组之间的数是可以重复的
                     //插入排序采用交换法
                     swap(arr, j, j - gap);
                     j -= gap;
